e135370dcff97337e94b9f58e4ca554944b0bdad
Dzmitry Markounikau <dzmitry.markounikau@lazada.com>
2018-07-13 22:15:17 +0700

    added product details

--- products/models.py
+++ products/models.py
@@ -1,3 +1,23 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+
+    def __str__(self):
+        return self.title
+
+    title = models.CharField(max_length=255)
+    body = models.TextField()
+    url = models.TextField()
+    pub_date = models.DateTimeField(auto_now_add=True)
+    image = models.ImageField(upload_to='images/')
+    icon = models.ImageField(upload_to='images/')
+    votes_total = models.IntegerField(default=1)
+    hunter = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def summary(self):
+        return "{} ...".format(self.body[:100])
+
+    def pub_date_pretty(self):
+        return self.pub_date.strftime('%b %e %Y')
--- products/views.py
+++ products/views.py
@@ -1,4 +1,44 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect, get_object_or_404
+from django.utils import timezone
+
+from products.models import Product
+
 
 def home(request):
     return render(request, 'products/home.html')
+
+
+@login_required
+def create(request):
+    if request.method == 'POST':
+        if request.POST.get('title') and request.POST.get('body') and request.POST.get('url') \
+                and request.FILES.get('icon') and request.FILES.get('image'):
+            request_url = request.POST.get('url')
+
+            product = Product()
+            product.title = request.POST.get('title')
+            product.body = request.POST.get('body')
+
+            if request_url.startswith('http://') or request_url.startswith('https://'):
+                product.url = request_url
+            else:
+                product.url = f'http://{request_url}'
+
+            product.image = request.FILES.get('image')
+            product.icon = request.FILES.get('icon')
+
+            product.pub_date = timezone.datetime.now()
+            product.hunter = request.user
+
+            product.save()
+            return redirect(f'/products/{product.id}')
+        else:
+            return render(request, 'products/create.html', {'error': 'All fields are required'})
+    else:
+        return render(request, 'products/create.html')
+
+
+def detail(request, product_id):
+    product = get_object_or_404(Product, pk=product_id)
+    return render(request, 'products/detail.html', {'product': product})
